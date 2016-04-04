0b724833aa9048851dbd9cf7f27fcfaa60bfaacf
unknown <>
2016-04-04 10:08:38 +0900

    initial commit

---
+++ public/login.php
@@ -0,0 +1,71 @@
+<?
+	include_once("include/head.php");
+?>  
+  <!-- Content -->
+  <div id="content"> 
+    
+    <!-- Revenues -->
+    <section class="check-out padding-top-15 padding-bottom-30">
+      <div class="container">
+		 <!-- Heading -->
+        <div class="heading text-left margin-bottom-20">
+          <h4>로그인</h4>
+        </div>
+        
+        <!-- coupen -->
+        <div class="coupen">
+          <p> 마케팅 플랫폼 <span>착한마케팅</span>에 오신것을 환영합니다.</p>
+        </div>
+
+		<!--div class="top_title">
+			<h4>로그인</h4>
+			<span>착한마케팅과 </span>
+		</div-->
+
+		<form>
+        <div class="row"> 
+          
+          <!-- Revenues Sidebar -->
+          <div class="col-md-8">
+			<!-- Story -->
+              <div class="story">
+			  <article>
+				<div class="login">
+					<label>아이디 *
+					<input class="form-control" type="text" name="" placeholder="">
+					</label>
+					<label>비밀번호 *
+					<input class="form-control" type="text" name="" placeholder="">
+					</label>
+					<a href="#." class="btn login_button margin-top-20">로그인<i class="fa fa-caret-right"></i></a>
+
+					
+				</div>
+			  </div>
+			  </article>
+          </div>
+          
+          <!-- Stories -->
+          <div class="col-md-4">
+              
+              <!-- Story -->
+              <div class="story">
+			  <article>
+				<div class="login_right">
+                    <label>페이스북으로 로그인해보세요!
+						<a class="btn-facebook btn-lg btn-block"><i class="fa fa-facebook"></i> 페이스북으로 로그인</a>
+					</label>
+					<p class="redirect01">아직 회원이 아니신가요? <a href="#"><strong>회원가입하기</strong></a></p>
+					<p class="redirect01">아이디,비밀번호를 잊으셨나요? <br/><a href="#"><strong>아이디,비밀번호 찾기</strong></a></p>
+
+                </div>
+              </article>
+              </div>
+        </div>
+      </div>
+    </section>
+  </div>
+  
+<?
+	include_once("include/footer.php");
+?>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,484 @@
+@extends('include.head')
+@section('content')
+  <!-- HOME MAIN SLIDER -->
+  <section class="home-slide">
+    <ul class="slides">
+      
+      <!-- SLIDER 1 -->
+      <li class="slide-img-1" data-stellar-background-ratio="0.6">
+        <div class="position-center-center">
+          <p class="main_banner_1">통합 IMC마케팅 / 브랜드관리 / SNS / 온라인마케팅</p>
+		  <p class="main_banner_1">영상 / 디자인 / 홈페이지 / 콘텐츠 제작 의뢰까지</p>
+
+		  
+		  <p class="main_banner_2">클라우드 마케팅 아웃소싱 플랫폼</p>
+          <p class="main_banner_3"></p><p class="b_txt01">광고주와 검증된 마케팅 대행사를 연결해 드립니다.</p>
+
+          <a href="#." class="btn">무료로 프로젝트 등록하기<i class="fa fa-caret-right"></i></a> </div>
+      </li>
+    </ul>
+  </section>
+
+
+  <!-- Counter -->
+    <section class="counter">
+      <div class="container"> 
+        
+        <!-- Team Member -->
+        <ul class="row">
+          <li class="col-md-3">
+            <div class="count"> <span class="number"> <span class="timer" data-speed="2000" data-refresh-interval="10" data-to="16812" data-from="0"></span> </span>
+			  <span style="font-size:16px;" class="b">건</span>
+              <h6 class="h6_txt">등록된 프로젝트</h6>
+            </div>
+          </li>
+          
+          <!-- Line Of Codes -->
+          <li class="col-md-6">
+            <div class="count"> <span class="number"> <span class="timer" data-speed="2000" data-refresh-interval="10" data-to="4820012302" data-from="0"></span> </span><span style="font-size:16px;" class="b">원</span>
+              <h6 class="h6_txt">프로젝트 등록금액</h6>
+            </div>
+          </li>
+          
+          <!-- Satisfied Client -->
+          <li class="col-md-3">
+            <div class="count"> <span class="number"> <span class="timer" data-speed="2000" data-refresh-interval="10" data-to="92" data-from="0"></span> </span><span style="font-size:16px;" class="b">명</span>
+              <h6 class="h6_txt">대행사 & 프리랜서</h6>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  
+  <!-- Content -->
+  <div id="content"> 
+    
+    <!-- SERVICES -->
+    <section class="light-gray-bg services padding-top-40 padding-bottom-30">
+      <div class="container"> 
+        <!-- Heading -->
+        <div class="heading text-center">
+          <h4 class="h4_txt">신규 프로젝트</h4>
+        </div>
+      </div>
+
+      <div class="best-services"> 
+        <!-- Row -->
+        <div class="container">
+          <ul class="row list">
+            <li class="col-md-4">
+              <article class="thumb">
+			  <a href="#"><h5>현대글로비스 공식 블로그 관리</h5></a>
+				<p class="price">\ 48,000,000  | 1년 | 21명 지원</p>
+
+                <p>현대글로비스 공식 블로그 연간 관리 대행사를 찾습니다. 연간 기획 및 운영 기획안을 포함한 제안서를 제출 하시면 내부검토후 1차 합격자만...</p>
+
+				<div id="tag">
+					<div class="button">blog</div>
+					<div class="button">페이스북</div>
+					<div class="button">홈페이지</div>
+				</div>
+				</a>
+                </article>
+            </li>
+            
+            <li class="col-md-4">
+              <article class="thumb">
+			  <a href="#"><h5>현대글로비스 공식 블로그 관리</h5></a>
+				<p class="price">\ 48,000,000  | 1년 | 21명 지원</p>
+
+                <p>현대글로비스 공식 블로그 연간 관리 대행사를 찾습니다. 연간 기획 및 운영 기획안을 포함한 제안서를 제출 하시면 내부검토후 1차 합격자만...</p>
+
+				<div id="tag">
+					<div class="button">blog</div>
+					<div class="button">페이스북</div>
+					<div class="button">홈페이지</div>
+				</div>
+				</a>
+                </article>
+            </li>
+            
+            <li class="col-md-4">
+              <article class="thumb">
+			 <a href="#"><h5>현대글로비스 공식 블로그 관리</h5></a>
+				<p class="price">\ 48,000,000  | 1년 | 21명 지원</p>
+
+                <p>현대글로비스 공식 블로그 연간 관리 대행사를 찾습니다. 연간 기획 및 운영 기획안을 포함한 제안서를 제출 하시면 내부검토후 1차 합격자만...</p>
+
+				<div id="tag">
+					<div class="button">blog</div>
+					<div class="button">페이스북</div>
+					<div class="button">홈페이지</div>
+				</div>
+				</a>
+                </article>
+            </li>
+            
+           <li class="col-md-4">
+              <article class="thumb">
+			  <a href="#"><h5>현대글로비스 공식 블로그 관리</h5></a>
+				<p class="price">\ 48,000,000  | 1년 | 21명 지원</p>
+
+                <p>현대글로비스 공식 블로그 연간 관리 대행사를 찾습니다. 연간 기획 및 운영 기획안을 포함한 제안서를 제출 하시면 내부검토후 1차 합격자만...</p>
+
+				<div id="tag">
+					<div class="button">blog</div>
+					<div class="button">페이스북</div>
+					<div class="button">홈페이지</div>
+				</div>
+				</a>
+                </article>
+            </li>
+            
+            <li class="col-md-4">
+              <article class="thumb">
+			  <a href="#"><h5>현대글로비스 공식 블로그 관리</h5></a>
+				<p class="price">\ 48,000,000  | 1년 | 21명 지원</p>
+
+                <p>현대글로비스 공식 블로그 연간 관리 대행사를 찾습니다. 연간 기획 및 운영 기획안을 포함한 제안서를 제출 하시면 내부검토후 1차 합격자만...</p>
+
+				<div id="tag">
+					<div class="button">blog</div>
+					<div class="button">페이스북</div>
+					<div class="button">홈페이지</div>
+				</div>
+				</a>
+                </article>
+            </li>
+            
+            <li class="col-md-4">
+              <article class="thumb">
+			  <a href="#"><h5>현대글로비스 공식 블로그 관리</h5></a>
+				<p class="price">\ 48,000,000  | 1년 | 21명 지원</p>
+
+                <p>현대글로비스 공식 블로그 연간 관리 대행사를 찾습니다. 연간 기획 및 운영 기획안을 포함한 제안서를 제출 하시면 내부검토후 1차 합격자만...</p>
+
+				<div id="tag">
+					<div class="button">blog</div>
+					<div class="button">페이스북</div>
+					<div class="button">홈페이지</div>
+				</div>
+				</a>
+                </article>
+            </li>
+          </ul>
+        </div>
+      </div>
+	</section>
+	
+
+	<!-- Plan -->
+    <section class="white-bg padding-top-30 padding-bottom-30">
+      <div class="container"> 
+        
+        <!-- Heading -->
+        <div class="heading text-center">
+          <h4>클라우드엠 프로세스</h4>
+        </div>
+        
+        <!-- Start Timeline -->
+        <div id="timeline" class="col-lg-10 col-md-12 col-sm-12">
+          <ul id="dates" class="custom-list">
+            <li><a href="#date1"><span>프로젝트 등록</span></a></li>
+            <li><a href="#date2"><span>지원자 비교</span></a></li>
+            <li><a href="#date3"><span>선택 미팅 및 계약</span></a></li>
+            <li><a href="#date4"><span>프로젝트 진행</span></a></li>
+            <li><a href="#date5"><span>프로젝트 완료</span></a></li>
+          </ul>
+          <ul id="issues" class="custom-list">
+            <li id="date1">
+              <div class="row">
+                <div class="history-thumbnail col-lg-5 col-lg-offset-1 col-md-6 col-md-offset-1 col-sm-7 col-sm-offset-0"> <img src="images/time-line-img.jpg" alt="" width="130"> </div>
+                <div class="history-content col-lg-5 col-md-5 col-sm-5">
+                  <h5 class="title">프로젝트 등록</h5>
+                  <p>현재 준비중인 광고 캠페인 및 콘텐츠 제작 프로젝트를 등록하면, 클라우드엠에 등록된 수많은 광고대행사 및 프리랜서들이 광고 캠페인 및 프로젝트를 살펴 보고, 견적서/제안서를 제출합니다.</p>
+                </div>
+              </div>
+            </li>
+            <li id="date2">
+              <div class="row">
+                <div class="history-thumbnail col-lg-5 col-lg-offset-1 col-md-6 col-md-offset-1 col-sm-7 col-sm-offset-0"> <img src="images/time-line-img.jpg" alt=""> </div>
+                <div class="history-content col-lg-5 col-md-5 col-sm-5">
+                  <h5 class="title">지원자 비교</h5>
+                  <p>지원자들의 포트폴리오와 다른 광고주들의 평판을 살펴 보십시요. 그리고 파트너가 제출한 견적서나 제안서를 살펴 보시고 마음에 드는 파트너를 선택하시면 됩니다.</p>
+                </div>
+              </div>
+            </li>
+            <li id="date3">
+              <div class="row">
+                <div class="history-thumbnail col-lg-5 col-lg-offset-1 col-md-6 col-md-offset-1 col-sm-7 col-sm-offset-0"> <img src="images/time-line-img.jpg" alt=""> </div>
+                <div class="history-content col-lg-5 col-md-5 col-sm-5">	
+                  <h5 class="title">선택 미팅 및 계약</h5>
+                  <p>마음에 드는 지원자를 선택하여 주시면 온라인/오프라인 미팅을 주선하여 드립니다. 미팅은 2회까지 가능합니다. 미팅 후 최종 선택을 하시면 클라우드엠에서 광고대행 계약서를 작성합니다.</p>
+                </div>
+              </div>
+            </li>
+            <li id="date4">
+              <div class="row">
+                <div class="history-thumbnail col-lg-5 col-lg-offset-1 col-md-6 col-md-offset-1 col-sm-7 col-sm-offset-0"> <img src="images/time-line-img.jpg" alt=""> </div>
+                <div class="history-content col-lg-5 col-md-5 col-sm-5">
+                  <h5 class="title">프로젝트 진행</h5>
+                  <p>계약서 작성 후 결제를 하시면 프로젝트가 진행됩니다. 진행되는 동안 클라우드엠은 광고주와 약속한 스케쥴대로 프로젝트가 진행되고 있는지 체크하고, 프로젝트가 제대로 마무리 될 수 있도록 중재하기도 합니다.</p>
+                </div>
+              </div>
+            </li>
+            <li id="date5">
+              <div class="row">
+                <div class="history-thumbnail col-lg-5 col-lg-offset-1 col-md-6 col-md-offset-1 col-sm-7 col-sm-offset-0"> <img src="images/time-line-img.jpg" alt=""> </div>
+                <div class="history-content col-lg-5 col-md-5 col-sm-5">
+                  <h5 class="title">프로젝트 완료</h5>
+                  <p>프로젝트는 광고주와 파트너가 모두 승인하는 경우에만 완료가 됩니다. 프로젝트가 완료되면 파트너에게 대행료를 입금하고 마무리 됩니다.</p>
+                </div>
+              </div>
+            </li>
+          </ul>
+          <a href="#" id="next"><i class="ion-ios-arrow-right"></i></a> <a href="#" id="prev"><i class="ion-ios-arrow-left"></i></a> </div>
+        <!-- End Timeline --> 
+        
+      </div>
+    </section>
+
+
+
+	
+	<section class="light-gray-bg padding-top-30 padding-bottom-20" >
+      <div class="container">
+        <div style="text-align:center;">
+         <p>광고 캠페인/영상/홈페이지/콘텐츠 제작 등 프로젝트를 찾고 있는 대행사/프리랜서인가요? <a href="#." class="btn btn-1" style="margin-left:30px;">회원가입하기<i class="fa fa-caret-right"></i></a></p>
+        </div>
+      </div>
+    </section>
+
+
+
+	  <!-- Features -->
+    <section class="features padding-top-30 padding-bottom-30">
+      <div class="container"> 
+        <!-- Heading -->
+        <div class="heading text-center">
+          <h4>빠르고 안정적인 클라우드엠</h4>
+        </div>
+        
+        <!-- Features -->
+        <div class="list-style-featured">
+          <div class="row no-margin"> 
+            <!-- LIST LEFT -->
+            <div class="col-md-4 no-padding">
+              <ul class="text-right">
+                <li>
+                  <div class="media">
+                    <div class="media-body">
+                      <h6>무료로 당신의 캠페인을 알리세요.</h6>
+                      <p>원하는 광고 프로젝트를 빠르고 안정되게 진행할 수 있습니다. 클라우드엠과 함께하는 수많은 파트너들에게 당신의 광고 캠페인을 알려보세요.</p>
+                    </div>
+                    <div class="media-right">
+                      <div class="m_icon01"> <!--<i class="fa fa-paint-brush"></i>--> </div>
+                    </div>
+                  </div>
+                </li>
+                <li>
+                  <div class="media">
+                    <div class="media-body">
+                      <h6>동영상/홈페이지/디자인/콘텐츠까지</h6>
+                      <p>광고와 관련된 영상/디자인/홈페이지/콘텐츠 제작까지 클라우드엠을 통해 해결하세요. 고도로 숙련된 프로들이 함께합니다.</p>
+                    </div>
+                    <div class="media-right">
+                      <div class="m_icon02"> <!--<i class="fa fa-tablet"></i>--> </div>
+                    </div>
+                  </div>
+                </li>
+                <li>
+                  <div class="media">
+                    <div class="media-body">
+                      <h6>실력있는 대행사를 빠르게 비교검색</h6>
+                      <p>다양한 경험을 가진 지원자들의 포트폴리오와 제안서. 함께 일했던 광고주들의 평가까지 한눈에 살펴보고 비교할 수 있습니다.</p>
+                    </div>
+                    <div class="media-right">
+                      <div class="m_icon03"> <!--<i class="fa fa-chrome"></i>--> </div>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            
+            <!-- LIST IMAGE -->
+            <div class="col-md-4 text-center no-padding"> <img class="img-responsive" src="images/iphone.png" alt=""> </div>
+            
+            <!-- LIST ICON RIGHT -->
+            <div class="col-md-4 no-padding">
+              <ul>
+                <li>
+                  <div class="media">
+                    <div class="media-left">
+                      <div class="m_icon04"> <!--<i class="fa fa-gear"></i>--> </div>
+                    </div>
+                    <div class="media-body">
+                      <h6>안전한 대금보호와 정확한 비용처리</h6>
+                      <p>에스크로 시스템을 통한 안전하게 대금을 보관하며, 진행하는 광고 캠페인 및 프로젝트가 정확히 마무리 되었을때만 비용을 지급합니다.</p>
+                    </div>
+                  </div>
+                </li>
+                <li>
+                  <div class="media">
+                    <div class="media-left">
+                      <div class="m_icon05"> <!--<i class="icon-layers"></i>--> </div>
+                    </div>
+                    <div class="media-body">
+                      <h6>세금계산서 및 계약서</h6>
+                      <p>세금계산서 발행 및 계약서 작성까지, 클라우드엠이 안전하고 꼼꼼하게 처리해 드립니다.</p>
+                    </div>
+                  </div>
+                </li>
+                <li>
+                  <div class="media">
+                    <div class="media-left">
+                      <div class="m_icon06"> <!--<i class="icon-bulb"></i>--> </div>
+                    </div>
+                    <div class="media-body">
+                      <h6>Innovative Ideas</h6>
+                      <p>Donec venenatis, turpis vel hendrerit interdum, dui ligula ultricies purus,  ultricies id orci.</p>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+      
+    
+    
+    <section class="light-gray-bg padding-top-30 padding-bottom-20" >
+      <div class="container">
+        <div style="text-align:center;">
+         <p>지금 준비중인 광고 캠페인이나 영상/디자인/홈페이지/콘텐츠 제작 프로젝트를 등록해 보세요. <a href="#." class="btn btn-1" style="margin-left:30px;">프로젝트 등록하기<i class="fa fa-caret-right"></i></a></p>
+        </div>
+      </div>
+    </section>
+    
+    
+
+	
+    <section class="white-bg blog padding-top-30 padding-bottom-30">
+      <div class="container"> 
+        <!-- Heading -->
+        <div class="heading text-center">
+          <h4>광고주들이 얘기하는 클라우드엠</h4>
+        </div>
+        <!-- Blog Row -->
+        <div class="row"> 
+          <!-- Blog Post -->
+          <div class="col-md-4 no-padding">
+            <article> <img class="img-responsive" src="images/m_pho01.png" alt="" > 
+              
+              
+              <!-- Detail -->
+              <div class="post-detail"> <a href="#." class="post-tittle">April stats Details</a> <span>By Admin  /  2 Comments</span>
+                <p>Donec venenatis, turpis vel hendrerit interdum, dui ligula ultricies purus, sed posuere libero dui id orci. Nam congue, pede vitae dapibus aliquet.</p>
+              </div>
+            </article>
+          </div>
+          <!-- Blog Post -->
+          <div class="col-md-4 no-padding">
+            <article> <img class="img-responsive" src="images/m_pho02.png" alt="" > 
+              <!-- Detail -->
+              <div class="post-detail"> <a href="#." class="post-tittle">EU LAWs vs US Laws</a> <span>By Admin  /  2 Comments</span>
+                <p>Donec venenatis, turpis vel hendrerit interdum, dui ligula ultricies purus, sed posuere libero dui id orci. Nam congue, pede vitae dapibus aliquet.</p>
+              </div>
+            </article>
+          </div>
+          <!-- Blog Post -->
+          <div class="col-md-4 no-padding">
+            <article> <img class="img-responsive" src="images/m_pho03.png" alt="" > 
+              
+              <!-- Detail -->
+              <div class="post-detail"> <a href="#." class="post-tittle">Team Expanded</a> <span>By Admin  /  2 Comments</span>
+                <p>Donec venenatis, turpis vel hendrerit interdum, dui ligula ultricies purus, sed posuere libero dui id orci. Nam congue, pede vitae dapibus aliquet.</p>
+              </div>
+            </article>
+          </div>
+          
+        </div>
+      </div>
+    </section>
+    
+    
+    <!-- Counter -->
+    <section class="clients  padding-top-30 padding-bottom-30">
+      <div class="container"> 
+        
+        <!-- Heading -->
+        <div class="heading text-center">
+          <h4>클라우드엠 협력업체</h4>
+          <span>법률조언, 분쟁중재 등 광고 프로젝트을 진행하며 발생 할 수 있는 수많은 변수들을 대비했습니다.</span>
+		</div>
+        
+			<!-- Clients -->
+			<div>
+			  <div class="item">
+				<ul class="row col-4">
+				  <li><img class="img-responsive" src="images/client/family1.jpg" alt=""></li>
+				  <li><img class="img-responsive" src="images/client/family2.jpg" alt=""></li>
+				  <li><img class="img-responsive" src="images/client/family1.jpg" alt=""></li>
+				  <li><img class="img-responsive" src="images/client/family1.jpg" alt=""></li>
+				</ul>
+			  </div>
+			</div>
+
+
+
+		<!-- Heading -->
+        <div class="heading text-center padding-top-70">
+          <h4>클라우드엠 클라이언트</h4>
+        </div>
+
+        <!-- Clients -->
+        <div>
+          <div class="item">
+            <ul class="row col-6">
+              <li><img class="img-responsive" src="images/client/family1.jpg" alt=""></li>
+			  <li><img class="img-responsive" src="images/client/family2.jpg" alt=""></li>
+			  <li><img class="img-responsive" src="images/client/family1.jpg" alt=""></li>
+			  <li><img class="img-responsive" src="images/client/family1.jpg" alt=""></li>
+			  <li><img class="img-responsive" src="images/client/family1.jpg" alt=""></li>
+			  <li><img class="img-responsive" src="images/client/family1.jpg" alt=""></li>
+            </ul>
+
+			<ul class="row col-6">
+              <li><img class="img-responsive" src="images/client/family1.jpg" alt=""></li>
+			  <li><img class="img-responsive" src="images/client/family2.jpg" alt=""></li>
+			  <li><img class="img-responsive" src="images/client/family1.jpg" alt=""></li>
+			  <li><img class="img-responsive" src="images/client/family1.jpg" alt=""></li>
+			  <li><img class="img-responsive" src="images/client/family1.jpg" alt=""></li>
+			  <li><img class="img-responsive" src="images/client/family1.jpg" alt=""></li>
+            </ul>
+
+			<ul class="row col-6">
+              <li><img class="img-responsive" src="images/client/family1.jpg" alt=""></li>
+			  <li><img class="img-responsive" src="images/client/family2.jpg" alt=""></li>
+			  <li><img class="img-responsive" src="images/client/family1.jpg" alt=""></li>
+			  <li><img class="img-responsive" src="images/client/family1.jpg" alt=""></li>
+			  <li><img class="img-responsive" src="images/client/family1.jpg" alt=""></li>
+			  <li><img class="img-responsive" src="images/client/family1.jpg" alt=""></li>
+            </ul>
+
+			<ul class="row col-6">
+              <li><img class="img-responsive" src="images/client/family1.jpg" alt=""></li>
+			  <li><img class="img-responsive" src="images/client/family2.jpg" alt=""></li>
+			  <li><img class="img-responsive" src="images/client/family1.jpg" alt=""></li>
+			  <li><img class="img-responsive" src="images/client/family1.jpg" alt=""></li>
+			  <li><img class="img-responsive" src="images/client/family1.jpg" alt=""></li>
+			  <li><img class="img-responsive" src="images/client/family1.jpg" alt=""></li>
+            </ul>
+          </div>
+        </div>
+
+
+
+      </div>
+    </section>
+  </div>
+  @include('include.footer')
+@endsection
