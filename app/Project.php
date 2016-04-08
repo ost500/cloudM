4a058bdaf0c1169d2ab97d514aed14d747889c27
OST <ost5253@gmail.com>
2016-04-08 11:19:48 +0900

    Create 대충 구현 다시 다시

---
+++ app/Project.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    //projects table의 모델이라고 명시해 준다.
+    protected $table = 'projects';
+    //
+}
