723245288bd413d597d693b24a46d6763cbd5d08
OST <ost5253@gmail.com>
2016-06-07 18:40:04 +0900

    deleteController added

---
+++ app/Http/Controllers/DeleteController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Application;
+use App\Project;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class DeleteController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    public function delete_project(Request $request)
+    {
+        $del_project = Project::find($request->id);
+        $del_project->delete();
+        
+        return redirect()->back();
+    }
+    public function delete_application(Request $request)
+    {
+        $del_app = Application::find($request->id);
+        $del_app->delete();
+
+        return redirect()->back();
+    }
+}
