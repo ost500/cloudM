d32820ef967d804884d2fc05b906ca3bca14483a
OST <ost5253@gmail.com>
2016-06-21 15:12:53 +0900

    프로젝트 검색 카테고리 업데이트2

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -92,7 +92,7 @@ class SearchController extends Controller
 
         $projects = new Collection();
         $area_menu_array = ['네이버CPC', '구글광고', '페이스북 스폰서광고', '매체 기타',
-            '네이버SEO', '언론보도', '컨텐츠 배포', '체험단 모집', ',바이럴 기타',
+            '네이버SEO', '언론보도', '컨텐츠 배포', '체험단 모집', '바이럴 기타',
             '블로그', '페이스북페이지', '기타SNS', '홈페이지', '운영대행 기타',
             '개발', '디자인', '웹툰', '영상', '1회성 프로젝트 기타'];
 
@@ -118,7 +118,7 @@ class SearchController extends Controller
 
                     foreach ($query as $q) {
                         if($q->step !='검수'){
-                            $projects = $projects->push($q->project);
+                            $projects = $projects->push($q);
                         }
                     }
                 }
