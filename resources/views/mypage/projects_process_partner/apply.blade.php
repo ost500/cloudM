3f93f1741f7711836f9681670b421b01dcdf60c5
jxamen <jxamen@gmail.com>
2016-06-27 14:16:44 +0900

    지원 프로젝트 목록 UI 수정1

--- resources/views/mypage/projects_process_partner/apply.blade.php
+++ resources/views/mypage/projects_process_partner/apply.blade.php
@@ -60,91 +60,81 @@
                         <div id="accordion">
 
                             <!-- Job Section -->
-                            <div class="job-content job-post-page">
-                                <!-- Job Tittle -->
+                            @if(sizeof($app) == 0)
+                                <td colspan="7">지원한 프로젝트가 없습니다</td>
+                            @endif
+                            @foreach($app as $appItem)
+                            <div class="job-content job-post-page margin-bottom-10">
                                 <div class="panel-group">
-                                    <div class="panel panel-default">
-                                        <!-- Save -->
-                                        <!--<div class="star-save"><a href="#."> <i class="fa fa-plus"></i></a><a href="#"><i class="fa fa-star"></i></a><a href="#"><i class="fa fa-link"></i></a> </div>-->
-                                        <!-- PANEL HEADING -->
-                                        <div class="panel-heading">
+                                    <div class="panel">
+                                        <div class="margin-bottom-10">
+                                            <a href="{{ url("detail/".$appItem->project->id) }}"><h6 class="my_h6 margin-bottom-10 margin-top-20">{{ $appItem->project->title }}</h6></a>
 
-                                            <a class="mypage_link" href="{{url('/services')}}">처음 오셨나요? 이용방법을 확인하세요
-                                                > </a>
-
-
-                                            <div class="job-tittle03 margin-bottom-10">
-                                                <h6 class="my_h6 margin-bottom-10 margin-top-20">지원한 프로젝트</h6>
-                                                <div class="panel02 panel-default02">
-                                                    <table class="table_01" width=100% cellpadding=0 cellspacing=0>
-                                                        <col style="width:25%;"/>
-                                                        <col style="width:12.5%;"/>
-                                                        <col style="width:12.5%;"/>
-                                                        <col style="width:12.5%;"/>
-                                                        <col style="width:7%;"/>
-                                                        <col style="width:7%;"/>
-                                                        <col style="width:7%;"/>
-                                                        <tr>
-                                                            <th>프로젝트 제목</th>
-                                                            <th>월예산</th>
-                                                            <th>기간</th>
-                                                            <th>마감</th>
-                                                            <th>서류</th>
-                                                            <th>상태</th>
-                                                            <th>취소</th>
-                                                        </tr>
-                                                        @if(sizeof($app) == 0)
-                                                            <td colspan="7">지원한 프로젝트가 없습니다</td>
-                                                        @endif
-                                                        @foreach($app as $appItem)
-
-                                                            <tr>
-                                                                <td><a
-                                                                            href="{{ url("detail/".$appItem->project->id) }}">{{ $appItem->project->title }}</a>
-                                                                </td>
-                                                                <td>{{ number_format($appItem->project->budget) }}</td>
-                                                                <td>{{ $appItem->project->estimated_duration }}</td>
-                                                                <td>{{ $appItem->project->deadline }}</td>
-                                                                <td>
-                                                                    <button type="button" class="btn btn-danger btn-xs"
-                                                                            data-toggle="modal" data-target="#fileModal"
-                                                                            data-pid="{{$appItem->project->id}}">관리
-                                                                    </button>
-                                                                </td>
-                                                                <td>{{ $appItem->choice }}</td>
-
-                                                                <form id="{{$appItem->id}}form" method="POST"
-                                                                      action="{{ url("/rm_app/") }}"
-                                                                      onsubmit="return confirm('취소하시겠습니까?');">
-                                                                    {!! csrf_field() !!}
-                                                                    <input name="id" hidden
-                                                                           value="{{$appItem->id}}">
-                                                                    <td>
-                                                                        <i style="cursor: pointer"
-                                                                           id="{{ $appItem->id }}button"
-                                                                           class="fa fa-times fa-lg"></i>
-                                                                    </td>
-                                                                </form>
-                                                                <script>
-                                                                    $("#{{$appItem->id}}button").click(function () {
-                                                                        $("#{{$appItem->id}}form").submit();
-                                                                    });
-                                                                </script>
-                                                            </tr>
-
+                                            <div>
+                                                <ul class="tags dall margin-top-10">
+                                                    <li>지역 > {{ str_limit($appItem->project->address_sido, 4, '') }} | 마케팅 분야
+                                                        @foreach($appItem->project->projects_area as $areas)
+                                                            <a href="#.">{{ $areas->area }}</a>
                                                         @endforeach
-                                                    </table>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                            <div class="panel02 panel-default02">
+                                                <table class="table_01" width=100% cellpadding=0 cellspacing=0>
+                                                    <col style="width:12.5%;"/>
+                                                    <col style="width:12.5%;"/>
+                                                    <col style="width:12.5%;"/>
+                                                    <col style="width:7%;"/>
+                                                    <tr>
+                                                        <th>월예산</th>
+                                                        <th>기간</th>
+                                                        <th>마감</th>
+                                                        <th>서류</th>
+                                                        {{--<th>취소</th>--}}
+                                                    </tr>
 
-                                                </div>
+
+                                                        <tr>
+                                                            </td>
+                                                            <td>{{ number_format($appItem->project->budget) }}</td>
+                                                            <td>{{ $appItem->project->estimated_duration }}</td>
+                                                            <td>{{ $appItem->project->deadline }}</td>
+                                                            <td>
+                                                                <button type="button" class="btn btn-danger btn-xs"
+                                                                        data-toggle="modal" data-target="#fileModal"
+                                                                        data-pid="{{$appItem->project->id}}">관리
+                                                                </button>
+                                                            </td>
+
+                                                            {{--<form id="{{$appItem->id}}form" method="POST"--}}
+                                                                  {{--action="{{ url("/rm_app/") }}"--}}
+                                                                  {{--onsubmit="return confirm('취소하시겠습니까?');">--}}
+                                                                {{--{!! csrf_field() !!}--}}
+                                                                {{--<input name="id" hidden--}}
+                                                                       {{--value="{{$appItem->id}}">--}}
+                                                                {{--<td>--}}
+                                                                    {{--<i style="cursor: pointer"--}}
+                                                                       {{--id="{{ $appItem->id }}button"--}}
+                                                                       {{--class="fa fa-times fa-lg"></i>--}}
+                                                                {{--</td>--}}
+                                                            {{--</form>--}}
+                                                            {{--<script>--}}
+                                                                {{--$("#{{$appItem->id}}button").click(function () {--}}
+                                                                    {{--$("#{{$appItem->id}}form").submit();--}}
+                                                                {{--});--}}
+                                                            {{--</script>--}}
+                                                        </tr>
+                                                </table>
                                             </div>
 
+                                            <p class="padding-top-10">{{ $appItem->content }}</p>
 
                                         </div>
                                     </div>
+
                                 </div>
                             </div>
-
-
+                            @endforeach
                         </div>
                     </div>
                 </div>
