c76aacec8472d91d414f98d01ddbbe14c4abf098
OST <ost5253@gmail.com>
2016-07-11 18:27:31 +0900

    인포그래픽 입력 정렬 필요

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -88,8 +88,14 @@
                                class="button002 signup002 margin-top-12">
                                 수정하기
                             </a>
-                            <div id="textarea_location"><?php echo nl2br($loginUser->partners['intro']); ?> </div>
-                            {{$errors->first('intro')}}
+                            @if($loginUser->partners->intro == null)
+                                <img class="img_cen" src="{{asset('images/empty_area.png')}}">
+                                <div class="clear"></div>
+                                <h7>자기소개가 없습니다</h7>
+                            @else
+                                <div id="textarea_location"><?php echo nl2br($loginUser->partners['intro']); ?> </div>
+                                {{$errors->first('intro')}}
+                            @endif
                         </div>
                         <script>
                             $("#intro_edit_button").click(function () {
@@ -111,24 +117,34 @@
                             <a href="{{ url('profile/portfolio/list/'.$loginUser->id) }}"
                                class="button002 signup002 margin-top-12">더 보기</a>
 
-                            <div class="row">
-
-                                @foreach($portfolios as $portfolio)
-                                    <div class="col-md-4">
-                                        <div class="thumbnail">
-                                            <div class="thum_imgbox">
-                                                <a href="{{ url('/profile/portfolio/'.$portfolio->id) }}"><img src="{{ $portfolio->image1 }}" alt="" class="img-responsive"></a>
-                                            </div>
-                                            <div class="caption">
-                                                <a href="{{ url('/profile/portfolio/'.$portfolio->id) }}"><h3 class="thum_title">{{ $portfolio->title }}</h3></a>
-                                                <p class="thum_category">{{ $portfolio->area }} > {{ $portfolio->category }}</p>
-                                                <p><a href="{{ url('/profile/portfolio/'.$portfolio->id) }}" class="btn btn-primary margin-top-10" role="button">자세히보기</a></p>
+                            <div class="row padding-left-15">
+                                @if($portfolios->isEmpty())
+                                    <img class="img_cen" src="{{asset('images/empty_portfolio.png')}}">
+                                    <div class="clear"></div>
+                                    <h7 style="text-align: center">포트폴리오가 없습니다</h7>
+                                @else
+                                    @foreach($portfolios as $portfolio)
+                                        <div class="col-md-4">
+                                            <div class="thumbnail">
+                                                <div class="thum_imgbox">
+                                                    <a href="{{ url('/profile/portfolio/'.$portfolio->id) }}"><img
+                                                                src="{{ $portfolio->image1 }}" alt=""
+                                                                class="img-responsive"></a>
+                                                </div>
+                                                <div class="caption">
+                                                    <a href="{{ url('/profile/portfolio/'.$portfolio->id) }}"><h3
+                                                                class="thum_title">{{ $portfolio->title }}</h3></a>
+                                                    <p class="thum_category">{{ $portfolio->area }}
+                                                        > {{ $portfolio->category }}</p>
+                                                    <p><a href="{{ url('/profile/portfolio/'.$portfolio->id) }}"
+                                                          class="btn btn-primary margin-top-10" role="button">자세히보기</a>
+                                                    </p>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
 
-                                @endforeach
-
+                                    @endforeach
+                                @endif
                             </div>
                         </div>
 
@@ -153,7 +169,8 @@
                                     <tbody id="skill_list">
                                     @if($loginUser->partners->job()->get()->isEmpty())
                                         <tr>
-                                            <td colspan="3">전문기술이 없습니다</td>
+                                            <td colspan="3"><img class="img_cen"
+                                                                 src="{{asset('images/empty_area.png')}}"><br>전문분야가 없습니다</td>
                                         </tr>
                                     @endif
                                     @foreach($loginUser->partners->job()->get() as $job)
@@ -219,7 +236,6 @@
                                                         <option value="외부광고">외부광고</option>
                                                         <option value="오프라인 기타">오프라인 기타</option>
                                                     </optgroup>
-
 
 
                                                 </select>
