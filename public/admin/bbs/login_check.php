2fa0f1fe1292aa4f1d1daac5197ef2b431dffd3d
OST <ost5253@gmail.com>
2016-07-11 11:30:52 +0900

    로그인 포인트 체크 주석처리

--- public/admin/bbs/login_check.php
+++ public/admin/bbs/login_check.php
@@ -45,14 +45,14 @@ set_session('ss_mb_key', md5($mb['mb_datetime'] . $_SERVER['REMOTE_ADDR'] . $_SE
 
 
 
-
-// 포인트 체크
-if($config['cf_use_point']) {
-    $sum_point = get_point_sum($mb['id']);
-
-    $sql= " update {$g5['member_table']} set mb_point = '$sum_point' where mb_id = '{$mb['id']}' ";
-    sql_query($sql);
-}
+//
+//// 포인트 체크
+//if($config['cf_use_point']) {
+//    $sum_point = get_point_sum($mb['id']);
+//
+//    $sql= " update {$g5['member_table']} set mb_point = '$sum_point' where mb_id = '{$mb['id']}' ";
+//    sql_query($sql);
+//}
 
 // 3.26
 // 아이디 쿠키에 한달간 저장
