dd1263eeefdb1bb122b524476439159f2f24f773
jxamen <jxamen@gmail.com>
2016-07-13 11:15:10 +0900

    어드민 및 파트너 체크수정, 로그분석 세팅

--- resources/views/include/head.blade.php
+++ resources/views/include/head.blade.php
@@ -12,6 +12,35 @@
     <meta property="og:image" content="http://fastm.app/images/preview.png">
     <meta property="og:description" content="마케팅 중개 플랫폼, 패스트엠에 오신 것을 환영합니다. 믿을 수 있는 광고대행사와 기업을 연결 해 드립니다.">
 
+    <meta name="naver-site-verification" content="a357861d27d02e4f068c4b1cc3daf41fce62cccc"/>
+
+    <!-- Facebook Pixel Code -->
+    <script>
+        !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+            n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+                document,'script','https://connect.facebook.net/en_US/fbevents.js');
+
+        fbq('init', '1699933600221154');
+        fbq('track', "PageView");</script>
+    <noscript><img height="1" width="1" style="display:none"
+                   src="https://www.facebook.com/tr?id=1699933600221154&ev=PageView&noscript=1"
+                /></noscript>
+    <!-- End Facebook Pixel Code -->
+
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-80709115-1', 'auto');
+        ga('send', 'pageview');
+
+    </script>
+
+
     <title>패스트엠::마케팅 중개 플랫폼</title>
 
     <!-- Favicon -->
