23f0cbdcd05f4448e0d2b8b7a232fac15c56a04d
jxamen <jxamen@gmail.com>
2016-07-20 15:02:50 +0900

    페북 로그인 테스트

---
+++ config/fb_auth.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jxamen
+ * Date: 2016-07-20
+ * Time: ¿ÀÈÄ 2:33
+ */
+return array(
+    "base_url"  => "http://fastm.io/fbauth/auth",
+    "providers" => array(
+        "Facebook"  => array(
+            "enabled"   => TRUE,
+            "keys"  => array("id" => "1755433851403442", "secret" => "9cd5bbe09b5286fdd278da31ab309b33"),
+            "scope" => "email"
+        )
+    )
+);
--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -11,6 +11,7 @@ return array(
     'Mockery' => array($vendorDir . '/mockery/mockery/library'),
     'JakubOnderka\\PhpConsoleHighlighter' => array($vendorDir . '/jakub-onderka/php-console-highlighter/src'),
     'JakubOnderka\\PhpConsoleColor' => array($vendorDir . '/jakub-onderka/php-console-color/src'),
+    'Hybrid' => array($vendorDir . '/hybridauth/hybridauth/hybridauth'),
     'Doctrine\\DBAL\\' => array($vendorDir . '/doctrine/dbal/lib'),
     'Doctrine\\Common\\Lexer\\' => array($vendorDir . '/doctrine/lexer/lib'),
     'Doctrine\\Common\\Inflector\\' => array($vendorDir . '/doctrine/inflector/lib'),
